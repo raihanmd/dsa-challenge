485d66087606732adf8b99aec67864a1f8be195d
raihanmd <mraihanf471@gmail.com>
2026-03-30 17:15:48 +0700

    leetcode: https://leetcode.com/problems/trapping-rain-water/description/ storage O(n)

--- leetcode/35. Trapping rain water/index.ts
+++ leetcode/35. Trapping rain water/index.ts
@@ -1,33 +1,28 @@
+// https://leetcode.com/problems/trapping-rain-water/description/
+
 function trap(height: number[]): number {
-  let res = 0;
+  let sum: number[] = [];
 
-  let maxLArr = [],
-    maxRArr = [],
-    minLR = [];
+  let l = 0,
+    r = height.length - 1,
+    maxL = height[l],
+    maxR = height[r];
 
-  let maxL = 0,
-    maxR = 0;
-
-  for (let i = 0; i < height.length; i++) {
-    maxLArr.push(maxL);
-    maxL = Math.max(maxL, height[i]);
+  while (l <= r) {
+    if (maxL <= maxR) {
+      const trapped = maxL - height[l];
+      sum[l] = trapped > 0 ? trapped : 0;
+      maxL = Math.max(maxL, height[l]);
+      l++;
+    } else {
+      const trapped = maxR - height[r];
+      sum[r] = trapped > 0 ? trapped : 0;
+      maxR = Math.max(maxR, height[r]);
+      r--;
+    }
   }
 
-  for (let i = height.length - 1; i >= 0; i--) {
-    maxRArr[i] = maxR;
-    maxR = Math.max(maxR, height[i]);
-  }
-
-  for (let i = 0; i < height.length; i++) {
-    minLR.push(Math.min(maxLArr[i], maxRArr[i]));
-  }
-
-  for (let i = 0; i < height.length; i++) {
-    const trapped = minLR[i] - height[i];
-    if (trapped > 0) res += trapped;
-  }
-
-  return res;
+  return sum.reduce((a, b) => a + b, 0);
 }
 
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
